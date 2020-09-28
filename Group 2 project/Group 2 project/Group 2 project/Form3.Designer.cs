5d1e9a5e8b4c527513b97e9651337dbec99e1eb4
i428672 <i428672@fhict.nl>
2020-09-28 12:35:16 +0200

    Multi-user login now working properly

--- Group 2 project/Group 2 project/Group 2 project/Form3.Designer.cs
+++ Group 2 project/Group 2 project/Group 2 project/Form3.Designer.cs
@@ -140,7 +140,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(-4, 86);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(523, 332);
+            this.pictureBox1.Size = new System.Drawing.Size(532, 332);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -185,7 +185,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(137, 26);
+            this.label4.Location = new System.Drawing.Point(137, 25);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(190, 23);
             this.label4.TabIndex = 4;

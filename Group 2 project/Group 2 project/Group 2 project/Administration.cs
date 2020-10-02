88a9a8aa2f03b3cf756efa84e59239a249d55b40
Sebastian Bokkestijn”git config --global user.name Sebastian <437602@student.fontys.nl>
2020-10-02 11:05:35 +0200

    Fixed Administration

--- Group 2 project/Group 2 project/Group 2 project/Administration.cs
+++ Group 2 project/Group 2 project/Group 2 project/Administration.cs
@@ -89,7 +89,8 @@ namespace Group_2_project
         private void btnAdd_Click(object sender, EventArgs e)
         {
             MySqlConnection conn = new MySqlConnection("Persist Security Info=False;database=dbi434661;server=studmysql01.fhict.local;Connect Timeout=30;user id=dbi434661; pwd=daivbot");
-            string query = "insert into dbi434661.employee(ID,Firstname,LastName,Age,Gender,DepartmentID,HireDate,Salary,Adress,Role)values('" + this.tbId.Text + "','" + this.tbFn.Text + "','" + this.tbSn.Text + "','" + this.tbAge.Text + "','" + this.tbGen.Text + "','" + this.tbDid.Text + "','" + this.dateTimePicker1.Text + "','" + this.tbSal.Text + "','" + this.tbAdd.Text + "','" + this.tbRole.Text + "');";
+            string query = "insert into dbi434661.employee(ID,Firstname,LastName,Age,Gender,DepartmentName" +
+                ",HireDate,EndDate,Salary,Adress,Role)values('" + this.tbId.Text + "','" + this.tbFn.Text + "','" + this.tbSn.Text + "','" + this.tbAge.Text + "','" + this.tbGen.Text + "','" + this.tbDid.Text + "','" + this.dateTimePicker1.Text + "','"+this.dateTimePicker2.Text+"''" + this.tbSal.Text + "','" + this.tbAdd.Text + "','" + this.tbRole.Text + "');";
             MySqlCommand command = new MySqlCommand(query, conn);
             MySqlDataReader reader;
 
@@ -116,7 +117,7 @@ namespace Group_2_project
         private void btnEdit_Click(object sender, EventArgs e)
         {
             MySqlConnection conn = new MySqlConnection("Persist Security Info=False;database=dbi434661;server=studmysql01.fhict.local;Connect Timeout=30;user id=dbi434661; pwd=daivbot");
-            string query = "update dbi434661.employee set ID='" + this.tbId.Text + "',FirstName='" + this.tbFn.Text + "',LastName='" + this.tbSn.Text + "',Age='" + this.tbAge.Text + "',Gender='" + this.tbGen.Text + "',DepartmentId='" + this.tbDid.Text + "',HireDate='" + this.dateTimePicker1.Text + "',Salary='" + this.tbSal.Text + "',Adress='" + this.tbAdd.Text + "',Role='" + this.tbRole.Text + "' where ID='" + this.tbId.Text + "' ;";
+            string query = "update dbi434661.employee set ID='" + this.tbId.Text + "',FirstName='" + this.tbFn.Text + "',LastName='" + this.tbSn.Text + "',Age='" + this.tbAge.Text + "',Gender='" + this.tbGen.Text + "',DepartmentName='" + this.tbDid.Text + "',HireDate='" + this.dateTimePicker1.Text + "',EndDate='"+this.dateTimePicker2.Text+"',Salary='" + this.tbSal.Text + "',Adress='" + this.tbAdd.Text + "',Role='" + this.tbRole.Text + "' where ID='" + this.tbId.Text + "' ;";
             MySqlCommand command = new MySqlCommand(query, conn);
             MySqlDataReader reader;
 
@@ -148,7 +149,7 @@ namespace Group_2_project
         {
             MySqlConnection conn = new MySqlConnection("Persist Security Info=False;database=dbi434661;server=studmysql01.fhict.local;Connect Timeout=30;user id=dbi434661; pwd=daivbot");
             string query = "delete from dbi434661.employee where ID='" + this.tbId.Text + "';";
-            string query2 = "delete from dbi434661.departments where DeptID='" + this.tbDid.Text + "';";
+            string query2 = "delete from dbi434661.departments where DeptName='" + this.tbDid.Text + "';";
             string query3 = "delete from dbi434661.login where eID='" + this.tbDid.Text + "';";
             MySqlCommand command = new MySqlCommand(query, conn);
             MySqlCommand command2 = new MySqlCommand(query2, conn);
